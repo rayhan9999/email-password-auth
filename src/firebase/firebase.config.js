c182c82b79f93cda2981796c4cc6f5aaacfd67d5
rayhan9999 <rayhanchowdhuri2023@gmail.com>
2023-04-15 19:36:50 +0600

    firebase learning

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDFnVzT5eTGfx03xTFfs8INGFR7eg7SfOI",
+    authDomain: "email-password-auth-4c4aa.firebaseapp.com",
+    projectId: "email-password-auth-4c4aa",
+    storageBucket: "email-password-auth-4c4aa.appspot.com",
+    messagingSenderId: "69294851482",
+    appId: "1:69294851482:web:1c800a52190c1a5b19e623"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/main.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Components/layout/Main';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+import RegisterRBS from './Components/RegisterRBS/RegisterRBS';
+import RegisterBS from './Components/RegisterBS/RegisterBS';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      },
+      {
+        path: '/register-rbs',
+        element: <RegisterRBS></RegisterRBS>
+      },
+      {
+        path: '/register-bs',
+        element: <RegisterBS></RegisterBS>
+      }
+    ]
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
